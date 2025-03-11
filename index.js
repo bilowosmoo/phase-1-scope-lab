5e533c19134451bdf5f619c706b13d3c2c416c97
bilow osmoo <bilowosmo@gmail.com>
2025-03-11 23:06:44 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"; // Declaring a global variable inside a function
+}
+
+// 4. Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "another person";
+}
